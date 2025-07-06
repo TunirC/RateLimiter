3b0eec41e81682401233d8a0642b89078fb922f7
root <root@LAPTOP-1ABJR90J.localdomain>
2025-07-06 19:42:00 +0000

    updated for sliding window log algorithm

---
+++ src/SlidingWindowLog/SlidingLogRateLimiter.java
@@ -0,0 +1,49 @@
+package SlidingWindowLog;
+
+import Core.RateLimiter;
+import Core.Resolver.KeyResolver;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class SlidingLogRateLimiter implements RateLimiter {
+
+    private final long windowSizeInSeconds;
+    private final int requestLimit;
+    private KeyResolver resolver;
+    private final Map<String, ConcurrentLinkedDeque<Long>> userBucket = new ConcurrentHashMap<>();
+
+    public SlidingLogRateLimiter(long windowSizeInSeconds, int requestLimit, KeyResolver resolver) {
+        this.windowSizeInSeconds = windowSizeInSeconds;
+        this.requestLimit = requestLimit;
+        this.resolver = resolver;
+    }
+
+    @Override
+    public boolean allowRequest(String userId, String ip, long incomingTimestamp) {
+        List<String> keys = resolver.resolveKey(userId, ip);
+        AtomicBoolean setAllowRequest = new AtomicBoolean(true);
+
+
+        for (String key: keys) {
+            ConcurrentLinkedDeque<Long> timestampQueue = userBucket.computeIfAbsent(key, (id) -> new ConcurrentLinkedDeque<>());
+
+            long offset = incomingTimestamp - windowSizeInSeconds;
+
+            while (!timestampQueue.isEmpty() && timestampQueue.peekFirst() < offset) {
+                timestampQueue.pollFirst();
+            }
+
+            if (timestampQueue.size() < requestLimit) {
+                timestampQueue.offerLast(incomingTimestamp);
+            } else {
+                setAllowRequest.set(false);
+            }
+        }
+
+        return setAllowRequest.get();
+    }
+}
